c879d2e7b5a98b5f28ae2f45a0eb71322360859b
John Skinner <jjcokeky@gmail.com>
2024-03-04 15:49:03 -0500

    In class example part 1

---
+++ Notes/JS/introToJavaScript/script.js
@@ -0,0 +1,29 @@
+console.log("My Script is running...");
+
+var myString = "Var: Intro to JavaScript";
+console.log(myString)
+let myString2 = "let: Intro to JavaScript"
+console.log(myString2)
+const myString3 = "Const: Intro to JavaScript"
+console.log(myString3)
+myString4 = ": Intro to JavaScript"
+console.log(myString)
+console.log(myString.toLowerCase())
+console.log(myString.toUpperCase())
+console.log(myString.length)
+// console.log(myString[2])
+// console.log(myString.includes("J"))
+// console.log(myString.replaceall("a","p"))
+
+let a = 5
+let b = 10
+
+console.log("a+b= " + a+b)
+
+let firstNumber = prompt("Please choose first number: ")
+
+let operand = package("Please choose one of the following: '+', '-'")
+
+let secondNumber = prompt("Please choose Second number: ")
+
+console.log(firstNumber + operand + secondNumber)
