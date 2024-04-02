92687f029066ae6359f060e16a09611687374292
John Skinner <jjcokeky@gmail.com>
2024-04-02 11:44:57 -0400

    added constants into java
    Added the array with pictures as well as the array with alt text

--- Assignment4/Part2/main.js
+++ Assignment4/Part2/main.js
@@ -5,8 +5,18 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const images = [`pic1.jpg`, `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+
+
 
 /* Declaring the alternative text for each image file */
+const altText = {
+    'pic1.jpg' : 'close up image of eye',
+    'pic2.jpg' : 'sandstone',
+    'pic3.jpg' : 'purple and white flowers',
+    'pic4.jpg' : 'old Egyption painting ',
+    'pic5.jpg' : 'butterfly on green leaf'
+}
 
 /* Looping through images */
 
