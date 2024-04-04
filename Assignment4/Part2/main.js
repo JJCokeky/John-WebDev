4a0359e48ad3fd6f45d6d07f492ae6dc0eb3bf1b
John Skinner <jjcokeky@gmail.com>
2024-04-04 12:21:34 -0400

    finished code
    Still need to add headers

--- Assignment4/Part2/main.js
+++ Assignment4/Part2/main.js
@@ -34,14 +34,14 @@ newImage.addEventListener('click', x => {
 /* Wiring up the Darken/Lighten button */
 
 btn.addEventListener('click', () => {
-    const btnClass = btn.getAttribute('class')
-    if (btnClass === 'dark') {
+    const buttonClass = btn.getAttribute('class')
+    if (buttonClass == 'dark') {
         btn.setAttribute('class','light');
         btn.textContent = 'Lighten';
-        overlay.style.backgroundColor = "rgb(0,0,0)";
+        overlay.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     } else {
         btn.setAttribute('class','dark');
         btn.textContent = 'Darken';
-        overlay.style.backgroundColor = "rgb(0,0,0 / 5%)";
+        overlay.style.backgroundColor = 'rgb(0 0 0 / 0%)';
     }
 });
